f352efc72bed65bea4ef72b0c03917c99f12cfc6
Dario Becchio <dabek1986@gmail.com>
2024-05-02 10:46:54 +0200

    create ComicsModel -crm

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*CRUD operations */
+
+Route::resource('comics' , ComicsController::class);
+
